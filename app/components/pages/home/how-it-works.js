cc1188f2c7d785f2905f51d21fb7c8380b1ff298
Mohamed-Elhawary <mohamed.k.elhawary@gmail.com>
2024-12-21 20:13:16 +0200

    chore: :dizzy: Add some framer motion animations in the home page

--- app/components/pages/home/how-it-works.js
+++ app/components/pages/home/how-it-works.js
@@ -1,9 +1,56 @@
+"use client";
+
+import { motion, useInView } from "framer-motion";
 import Image from "next/image";
+import { useRef } from "react";
 
 export default function HomeHowItWorks() {
+    const ref = useRef(null);
+
+    const isInView = useInView(
+        ref,
+        { once: true },
+    );
+
+    const steps = [
+        {
+            alt: "step_1",
+            id: 1,
+            imageSrc: "/images/pages/home/how-it-works/step_1.png",
+        },
+        {
+            alt: "step_2",
+            id: 2,
+            imageSrc: "/images/pages/home/how-it-works/step_2.png",
+        },
+        {
+            alt: "step_3",
+            id: 3,
+            imageSrc: "/images/pages/home/how-it-works/step_3.png",
+        },
+        {
+            alt: "step_4",
+            id: 4,
+            imageSrc: "/images/pages/home/how-it-works/step_4.png",
+        },
+    ];
+
     return (
-        <div className="container px-4 md:px-6 mx-auto pt-20 md:pt-40 pb-[260px] lg:pb-[520px]">
-            <div>
+        <div
+            className="container px-4 md:px-6 mx-auto pt-20 md:pt-40 pb-[260px] lg:pb-[520px]"
+            ref={ref}
+        >
+            <motion.div
+                animate={isInView && { y: 0 }}
+                initial={{ y: "100vh" }}
+                transition={{
+                    damping: 10,
+                    duration: 2,
+                    ease: "easeInOut",
+                    stiffness: 50,
+                    type: "spring",
+                }}
+            >
                 <div className="mb-14 md:mb-28">
                     <h2 className="font-normal text-3xl md:text-5xl text-center leading-relaxed">
                         How it works?
@@ -13,40 +60,25 @@ export default function HomeHowItWorks() {
                     </h2>
                 </div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-12 gap-4 md:gap-8 justify-items-center md:justify-items-start">
-                    <div className="col-span-1 md:col-span-3">
-                        <Image
-                            alt="step_1"
-                            height={390}
-                            src="/images/pages/home/how-it-works/step_1.png"
-                            width={288}
-                        />
-                    </div>
-                    <div className="col-span-1 md:col-span-3">
-                        <Image
-                            alt="step_2"
-                            height={390}
-                            src="/images/pages/home/how-it-works/step_2.png"
-                            width={288}
-                        />
-                    </div>
-                    <div className="col-span-1 md:col-span-3">
-                        <Image
-                            alt="step_3"
-                            height={390}
-                            src="/images/pages/home/how-it-works/step_3.png"
-                            width={288}
-                        />
-                    </div>
-                    <div className="col-span-1 md:col-span-3">
-                        <Image
-                            alt="step_4"
-                            height={390}
-                            src="/images/pages/home/how-it-works/step_4.png"
-                            width={288}
-                        />
-                    </div>
+                    {steps.map(({
+                        alt,
+                        id,
+                        imageSrc,
+                    }) => (
+                        <div
+                            className="col-span-1 md:col-span-3 transform transition-transform duration-500 hover:scale-110"
+                            key={id}
+                        >
+                            <Image
+                                alt={alt}
+                                height={390}
+                                src={imageSrc}
+                                width={288}
+                            />
+                        </div>
+                    ))}
                 </div>
-            </div>
+            </motion.div>
             <div className="pt-20 md:pt-40">
                 <h2 className="font-semibold text-4xl md:text-6xl uppercase text-center !leading-relaxed">
                     Building a Legacy, One
--- app/components/pages/home/successful-stories.js
+++ app/components/pages/home/successful-stories.js
@@ -1,39 +1,55 @@
 "use client";
 
+import { motion, useInView } from "framer-motion";
+import Image from "next/image";
+import { useRef } from "react";
 import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 export default function HomeSuccessfulStories() {
+    const ref = useRef(null);
+
+    const isInView = useInView(
+        ref,
+        { once: true },
+    );
+
     const slides = [
         {
+            alt: "story_1",
             id: 1,
-            imageUrl: "/images/pages/home/successful-stories/story_1.png",
-            title: "MARAKEZ | DISTRICT 1",
+            imageSrc: "/images/pages/home/successful-stories/story_1.png",
+            title: "Marakez | District 1",
         },
         {
+            alt: "story_2",
             id: 2,
-            imageUrl: "/images/pages/home/successful-stories/story_2.png",
-            title: "MARAKEZ | DISTRICT 2",
+            imageSrc: "/images/pages/home/successful-stories/story_2.png",
+            title: "Marakez | District 2",
         },
         {
+            alt: "story_3",
             id: 3,
-            imageUrl: "/images/pages/home/successful-stories/story_3.png",
-            title: "MARAKEZ | DISTRICT 3",
+            imageSrc: "/images/pages/home/successful-stories/story_3.png",
+            title: "Marakez | District 3",
         },
         {
+            alt: "story_4",
             id: 4,
-            imageUrl: "/images/pages/home/successful-stories/story_4.png",
-            title: "MARAKEZ | DISTRICT 4",
+            imageSrc: "/images/pages/home/successful-stories/story_4.png",
+            title: "Marakez | District 4",
         },
         {
+            alt: "story_5",
             id: 5,
-            imageUrl: "/images/pages/home/successful-stories/story_5.png",
-            title: "MARAKEZ | DISTRICT 5",
+            imageSrc: "/images/pages/home/successful-stories/story_5.png",
+            title: "Marakez | District 5",
         },
         {
+            alt: "story_6",
             id: 6,
-            imageUrl: "/images/pages/home/successful-stories/story_6.png",
-            title: "MARAKEZ | DISTRICT 6",
+            imageSrc: "/images/pages/home/successful-stories/story_6.png",
+            title: "Marakez | District 6",
         },
     ];
 
@@ -47,29 +63,46 @@ export default function HomeSuccessfulStories() {
                     Stories
                 </h2>
             </div>
-            <Swiper
-                className="stories-slider"
-                modules={[Navigation]}
-                pagination={{ clickable: true }}
-                slidesPerView={3}
-                spaceBetween={20}
-                navigation
+            <motion.div
+                animate={isInView && { opacity: 1 }}
+                initial={{ opacity: 0 }}
+                ref={ref}
+                transition={{
+                    duration: 2,
+                    ease: "easeInOut",
+                }}
             >
-                {slides.map((slide) => (
-                    <SwiperSlide key={slide.id}>
-                        <div className="rounded-lg border-white border-[1px] p-4 w-[395] h-[431px]">
-                            <img
-                                alt={slide.title}
-                                className="rounded-lg object-cover w-full h-[317px]"
-                                src={slide.imageUrl}
-                            />
-                            <div className="text-white mt-4">
-                                <h3>{slide.title}</h3>
+                <Swiper
+                    className="stories-slider"
+                    modules={[Navigation]}
+                    pagination={{ clickable: true }}
+                    slidesPerView={3}
+                    spaceBetween={20}
+                    navigation
+                >
+                    {slides.map(({
+                        alt,
+                        id,
+                        imageSrc,
+                        title,
+                    }) => (
+                        <SwiperSlide key={id}>
+                            <div className="rounded-lg border-white border-[1px] p-4 w-[395] h-[431px]">
+                                <div className="group overflow-hidden rounded-lg">
+                                    <Image
+                                        alt={alt}
+                                        className="rounded-lg object-cover w-full transition-transform duration-500 group-hover:scale-150"
+                                        height={317}
+                                        src={imageSrc}
+                                        width={347}
+                                    />
+                                </div>
+                                <h5 className="font-normal text-base md:text-xl text-white mt-4 !font-nanum-myeongjo">{title}</h5>
                             </div>
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </motion.div>
         </div>
     );
 }
